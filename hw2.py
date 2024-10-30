520d9525df516aaaf41a6af40c848d04481599be
ERR11CCG <eric0617garcia@gmail.com>
2024-10-30 13:15:33 -0700

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -3,7 +3,14 @@ import data
 # Write your functions for each part in the space below.
 
 # Part 1
-
+def create_rectangle(p1: Point, p2: Point) -> Rectangle:
+    top_left_x = min(p1.x, p2.x)
+    top_left_y = max(p1.y, p2.y)
+    bottom_right_x = max(p1.x, p2.x)
+    bottom_right_y = min(p1.y, p2.y)
+    top_left = Point(top_left_x, top_left_y)
+    bottom_right = Point(bottom_right_x, bottom_right_y)
+    return Rectangle(top_left, bottom_right)
 
 # Part 2
 
