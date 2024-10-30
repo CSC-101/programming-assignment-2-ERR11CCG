9224a1e7b96363fb87272c0d2576ea9ac4e15683
ERR11CCG <eric0617garcia@gmail.com>
2024-10-30 14:24:50 -0700

    Update hw2_tests.py

--- hw2_tests.py
+++ hw2_tests.py
@@ -28,9 +28,32 @@ print(shorter_duration_than(d3, d4))
 
 
     # Part 3
+song1 = Song("Artist1", "Song1", Duration(3, 30))
+song2 = Song("Artist2", "Song2", Duration(2, 45))
+song3 = Song("Artist3", "Song3", Duration(4, 10))
+songs_list = [song1, song2, song3]
+
+max_duration = Duration(3, 0)
+
+short_songs = songs_shorter_than(songs_list, max_duration)
+print(short_songs)
 
 
     # Part 4
+song1 = Song("Decemberists", "June Hymn", Duration(4, 30))
+song2 = Song("Broken Bells", "October", Duration(3, 40))
+song3 = Song("Kansas", "Dust in the Wind", Duration(3, 29))
+song4 = Song("Local Natives", "Airplanes", Duration(3, 58))
+
+songs_list = [song1, song2, song3, song4]
+
+
+playlist = [0, 2, 1, 3, 0]  
+
+
+total_duration = running_time(songs_list, playlist)
+print(total_duration)
+ 
 
 
     # Part 5
