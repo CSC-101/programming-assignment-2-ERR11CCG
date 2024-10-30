3df604e5fc2a01e77910d89f86350d8b877b5059
ERR11CCG <eric0617garcia@gmail.com>
2024-10-30 13:23:45 -0700

    Update hw2_tests.py

--- hw2_tests.py
+++ hw2_tests.py
@@ -18,6 +18,13 @@ rectangle = create_rectangle(p1, p2)
 print(rectangle)
 
     # Part 2
+d1 = Duration(3, 20)  
+d2 = Duration(4, 0)   
+print(shorter_duration_than(d1, d2))  
+
+d3 = Duration(5, 0)
+d4 = Duration(4, 30)
+print(shorter_duration_than(d3, d4))  
 
 
     # Part 3
